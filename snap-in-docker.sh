7c29cab3733947db6ad188a938b5ebcbc90214c6
Leo Arias <leo.arias@canonical.com>
2016-11-25 01:52:21 +0000

    Try with bzr

--- snap-in-docker.sh
+++ snap-in-docker.sh
@@ -9,4 +9,4 @@ set -ev
 
 repo="$1"
 
-docker run -v "$(pwd)":/cwd ubuntu:xenial sh -c "apt update && apt install git python3 python3-docopt snapcraft -y && cd /cwd && python3 -m external_snaps_tests $repo"
+docker run -v "$(pwd)":/cwd ubuntu:xenial sh -c "apt update && apt install bzr git python3 python3-docopt snapcraft -y && cd /cwd && python3 -m external_snaps_tests $repo"
