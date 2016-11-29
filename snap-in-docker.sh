ab344d9b3223b07e2665855bf922316ba4333998
Leo Arias <yo@elopio.net>
2016-11-28 23:41:15 -0600

    Add darktable (#11)

--- snap-in-docker.sh
+++ snap-in-docker.sh
@@ -9,4 +9,4 @@ set -ev
 
 repo="$1"
 
-docker run -v "$(pwd)":/cwd ubuntu:xenial sh -c "apt update && apt install git python3 python3-docopt snapcraft -y && cd /cwd && python3 -m external_snaps_tests $repo"
+docker run -v "$(pwd)":/cwd ubuntu:xenial sh -c "apt update && apt install git locales python3 python3-docopt snapcraft -y && locale-gen en_US.UTF-8 && export LC_ALL=en_US.UTF-8 && cd /cwd && python3 -m external_snaps_tests $repo"
