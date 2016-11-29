70b5790596b30c740f79c65e20c81713b99e6cbf
Leo Arias <yo@elopio.net>
2016-11-29 17:53:04 -0600

    Add lxd (#12)

--- snap-in-docker.sh
+++ snap-in-docker.sh
@@ -3,10 +3,16 @@
 # Build a snap using a docker container.
 #
 # Arguments:
-#   repo: The URL of the repository to build.
+#   repo:       The URL of the repository to build.
+#   repo-brach: The branch of the repository to build. If it is not passed, the
+#               default branch will be built.
 
 set -ev
 
 repo="$1"
 
-docker run -v "$(pwd)":/cwd ubuntu:xenial sh -c "apt update && apt install git locales python3 python3-docopt snapcraft -y && locale-gen en_US.UTF-8 && export LC_ALL=en_US.UTF-8 && cd /cwd && python3 -m external_snaps_tests $repo"
+if [ ! -z "$2" ]; then
+    repo_branch="--repo-branch $2"
+fi
+
+docker run -v "$(pwd)":/cwd ubuntu:xenial sh -c "apt update && apt install git locales python3 python3-docopt snapcraft -y && locale-gen en_US.UTF-8 && export LC_ALL=en_US.UTF-8 && cd /cwd && python3 -m external_snaps_tests $repo $repo_branch"
