a3831408a0f7b7b598a6cfa50d7e3f6a1c9cd010
Leo Arias <leo.arias@canonical.com>
2017-07-05 04:59:05 +0000

    fix the perrmissions

--- snap-in-docker.sh
+++ snap-in-docker.sh
@@ -14,4 +14,4 @@ repo="$1"
 repo_branch="$2"
 ppa="$3"
 
-docker run --privileged -v "$(pwd)":/snapcraft -v "$TRAVIS_BUILD_DIR":/build -v "$TRAVIS_BUILD_DIR":/tmp ubuntu:xenial sh -c "apt update && apt install locales -y && locale-gen en_US.UTF-8 && export LC_ALL=en_US.UTF-8 && cd /snapcraft && apt build-dep -y ./ && /build/snap.sh $repo $repo_branch $ppa"
+docker run --privileged -v "$(pwd)":/snapcraft -v "$TRAVIS_BUILD_DIR":/build -v "$TRAVIS_BUILD_DIR":/tmp ubuntu:xenial sh -c "chmod 777 /tmp && apt update && apt install locales -y && locale-gen en_US.UTF-8 && export LC_ALL=en_US.UTF-8 && cd /snapcraft && apt build-dep -y ./ && /build/snap.sh $repo $repo_branch $ppa"
